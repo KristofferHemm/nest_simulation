e24e3151728d307c861dd853c41efa96be22266a
Kristoffer Hemm <kristoffer.hemm@gmail.com>
2024-04-19 10:33:22 +0200

    added more comments to scripts

--- params.py
+++ params.py
@@ -1,29 +1,35 @@
 import pandas as pd
 import numpy as np
-import json
 
-ts = np.array([10, 15, 19])
-tm = np.array([185, 190, 195])
-th = np.array([0.5, 5.0, 10.0])
+ts = np.array([10, 15, 19]) # synaptic time constant 
+tm = np.array([185, 190, 195]) # time constant of membrane potential in ms
+th = np.array([0.5, 5.0, 10.0]) # spike threshold
 
-placeholder = []
+paramslist = []
 
+# Creating nested lists for all parameter combinations
 for i in ts:
     for j in tm:
         for k in th:
-            placeholder.append(np.array([i, j, k]))
+            paramslist.append(np.array([i, j, k]))
 
-df1 = pd.DataFrame(placeholder, columns=['tauSyn', 'tauMem', 'theta'])
-df = pd.DataFrame()
+# Place all parameter combinations in a datafram
+df_noseed = pd.DataFrame(paramslist, columns=['tauSyn', 'tauMem', 'theta'])
+
+# Stack the noseed dataframe 5 times to prepare simulations using 5 different seeds
+df_seed = pd.DataFrame()
 for i in range(5):
-    df = df.append(df1, ignore_index=True)
+    df_seed = df_seed.append(df_noseed, ignore_index=True)
 
+# Create list of seeds in increments of 123
 seed = []
 for i in range(5):
-    for j in range(len(df1)):
+    for j in range(len(df_noseed)):
         seed.append(1234 + 123 * i)
 
-df['seed'] = seed
+# Insert seeds into the dataframe
+df_seed['seed'] = seed
 
-df.to_csv('params.csv', index=False)
+# Save dataframe as csv file
+df_seed.to_csv('params.csv', index=False)
 
--- run.py
+++ run.py
@@ -7,31 +7,35 @@ import pyarrow.parquet as pq
 from concurrent.futures import ProcessPoolExecutor
 
 def run_brunel(params_dict):
+    """
+    Run the simulation in NEST
+    Make sure that the number of cores is set equal to max_workers 
+    Make sure that the NEST script is correct
+    """
     subprocess.run(['srun', '-c', '4', 'python', 'brunel_alpha_nest.py', f'{params_dict}'])
     return f'{params_dict}'
 
 def main():
-    results = {}
-    params_df = pd.read_csv('.../params.csv')
-    params_dict = params_df.to_dict(orient='records')
+    results = {} # dictionary for holding results
+    params_df = pd.read_csv('.../params.csv') # read parameters
+    params_dict = params_df.to_dict(orient='records') #transform parameters to dictionary
 
-    with ProcessPoolExecutor(max_workers=4) as executor:
-        for params in executor.map(run_brunel, params_dict):
-            json_acceptable_string = params.replace("'", "\"")
-            param = json.loads(json_acceptable_string)
+    with ProcessPoolExecutor(max_workers=4) as executor: # max workers must equal cpus per task in run.slurm
+        for params in executor.map(run_brunel, params_dict): # run simulation for each set of parameters provided
+            json_acceptable_string = params.replace("'", "\"") 
+            param = json.loads(json_acceptable_string) # parameters must be in json format for the NEST script
             new_tauSyn, new_tauMem, new_theta, new_seed =  param.values()
-            fname = f'ban_{new_tauSyn}_{new_tauMem}_{new_theta}_{new_seed}.parquet'
-            table = pq.read_table(fname)
-            results[params] = table
+            fname = f'ban_{new_tauSyn}_{new_tauMem}_{new_theta}_{new_seed}.parquet' # filename for each individual simulation
+            table = pq.read_table(fname) # save the results returned from the simulation
+            results[params] = table # save results in results dictionary
             print('.', end='')
 
-
+    # Pickle the data dictionary using the highest protocol available
     with open('ban_all.pickle', 'wb') as f:
-        # Pickle the 'data' dictionary using the highest protocol available.
         experiment = {'parameters': params_df, 'results': results}
         pickle.dump(experiment, f, pickle.HIGHEST_PROTOCOL)
     
-    # Slette parquet filer
+    # Remove parquet files after all data have been pickled
     if os.path.isfile('ban_all.pickle'):
         dir_name = os.getcwd()
         test = os.listdir(dir_name)
